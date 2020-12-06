a524bfbf4e17fa073adff0b392afe0e916d31ed1
Ryan <Hoodrn03@gmail.com>
2020-12-06 19:36:28 +0000

    New Build

--- Game Prototype/Jingle Game Jam 2020/Assets/Scripts/Player Functions/Player_Spawn.cs
+++ Game Prototype/Jingle Game Jam 2020/Assets/Scripts/Player Functions/Player_Spawn.cs
@@ -19,8 +19,6 @@ public class Player_Spawn : MonoBehaviour
 
     public void m_SpawnPlayer()
     {
-        m_PlayerSpawnPoint = GameObject.FindGameObjectWithTag("Player Spawn Point");
-
         if (m_PlayerSpawnPoint != null)
         {
             m_PlayerObj.transform.position = m_PlayerSpawnPoint.transform.position; 
@@ -29,8 +27,6 @@ public class Player_Spawn : MonoBehaviour
 
     public void m_SpawnPlayer(GameObject objToSpawn)
     {
-        m_PlayerSpawnPoint = GameObject.FindGameObjectWithTag("Player Spawn Point");
-
         if (m_PlayerSpawnPoint != null)
         {
             objToSpawn.transform.position = m_PlayerSpawnPoint.transform.position;
